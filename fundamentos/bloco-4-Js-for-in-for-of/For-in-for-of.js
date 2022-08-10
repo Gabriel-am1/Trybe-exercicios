cc9a8c67b52562a3d8f66f33e1c49983dc4acbf6
Gabriel Amaral <gabriel.amaralll1995@gmail.com>
2022-08-10 15:59:57 -0300

    Realizando alguns exercicios

--- fundamentos/bloco-4-Js-for-in-for-of/For-in-for-of.js
+++ fundamentos/bloco-4-Js-for-in-for-of/For-in-for-of.js
@@ -1,4 +1,4 @@
-// Exercicios For in 
+// Exercicios Objetos
 
 let diasDaSemana = {
    1 : 'domingo',
@@ -9,8 +9,169 @@ let diasDaSemana = {
    6 : 'sexta',
    7 : 'sábado',
     };
-    
-    // diasDaSemana.1; // SyntaxError: Unexpected number
-    console.log(diasDaSemana['1']); // domingo
 
-    
+    // diasDaSemana.1; // SyntaxError: Unexpected number
+    // console.log(diasDaSemana['1']); // domingo
+    console.log(diasDaSemana['1']);
+
+// -------------------------------------------------------------------
+
+
+let conta = {
+    agencia: '0000',
+    banco: {
+      cod: '012',
+      id: 4,
+      nome: 'TrybeBank',
+    },
+  };
+
+  let infoDoBanco = 'banco';
+//   console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+  console.log(conta[infoDoBanco]['nome']); // TrybeBank
+
+  console.log(conta.agencia); // 0000
+  console.log(conta['agencia']); // 0000
+
+  console.log(conta.banco.cod); // 012
+  console.log(conta['banco']['id']); // 4
+
+// --------------------------------------------------------------------------------
+
+let usuario = {
+    id: 99,
+    email: 'jakeperalta@gmail.com',
+    infoPessoal: {
+      nome: 'Jake',
+      sobrenome: 'Peralta',
+      endereco: {
+        rua: 'Smith Street',
+        bairro: 'Brooklyn',
+        cidade: 'Nova Iorque',
+        estado: 'Nova Iorque',
+      },
+    },
+  };
+
+  console.log(usuario['id']); // 99
+  console.log(usuario.email); // jakeperalta@gmail.com
+
+  console.log(usuario.infoPessoal.endereco.rua); // Smith Street
+  console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
+
+// ------------------------------------------------------------------------------------
+
+let moradores = [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ];
+
+  let primeiroMorador = moradores[0];
+  console.log(primeiroMorador); // { nome: 'Luiza', sobrenome: 'Guimarães', andar: 10, apartamento: 1005 }
+  console.log(primeiroMorador['andar']); // 10
+
+  let ultimoMorador = moradores[moradores.length - 1];
+  console.log(ultimoMorador); // { nome: 'Zoey', sobrenome: 'Brooks', andar: 1, apartamento: 101 }
+  console.log(ultimoMorador.nome); // Zoey
+
+
+/*MÃO NA MASSA*/
+
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+let bestInTheWorld = [2006, 2007,2008,2009,2010,2018];
+
+let player = {
+ name: 'Marta',
+ lastName: 'Silva',
+ age: 34,
+ medals: { golden: 2, silver: 3 }
+};
+
+
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName  + ' ' +'tem' + ' ' + player.age + ' ' + 'de idade')
+
+player.bestInTheWorld = [2006, 2007,2008,2009,2010,2018],
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+
+console.log('A jogadora possui' + ' ' + player.medals.golden + ' ' + 'de ouro' + ' ' + 'e' + ' ' + player.medals.silver + ' ' + 'de prata' )
+
+
+// ------------ EXERCÍCIOS FOR IN ------------------------------
+
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+ for (let key in names ){
+    console.log('Olá' + ' ' + names[key])
+ }
+
+
+//  --------------------------------------------------------------
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+  for (let key in car) {
+   console.log(key + ': ' + car[key]);
+}
+
+
+
+// ----------------- EXERCÍCIOS FUNÇÕES -------------------------------
+
+
+function soma(a, b) {
+    return a + b;
+  }
+  
+  function sub(a, b) {
+    return a - b;
+  }
+  
+  function mult(a, b) {
+    return a * b;
+  }
+  
+  function div(a, b) {
+    return a / b;
+  }
+  
+  function mod(a, b) {
+    return a % b;
+  }
+
+  
